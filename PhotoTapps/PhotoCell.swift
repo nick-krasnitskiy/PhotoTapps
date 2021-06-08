71e1bb606d1e67c9526485593e1006f2b0235c07
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-06-08 15:25:20 +0300

    Collection View configuration was implemented.

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Nick Krasnitskiy on 08.06.2021.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
---
+++ PhotoTapps/PhotosCollectionViewController.swift
@@ -0,0 +1,49 @@
+//
+//  PhotosCollectionViewController.swift
+//  PhotoTapps
+//
+//  Created by Nick Krasnitskiy on 08.06.2021.
+//
+
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 15
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
+        cell.backgroundColor = .black
+        return cell
+    }
+    
+}
+
+extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+}
