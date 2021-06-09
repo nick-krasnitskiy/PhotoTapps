8194b3d851bd26199f1b6e98a204da85d670146e
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-06-09 14:42:57 +0300

    Photo sharing was added.

--- PhotoTapps/PhotoViewController.swift
+++ PhotoTapps/PhotoViewController.swift
@@ -18,6 +18,15 @@ class PhotoViewController: UIViewController {
     }
     
     @IBAction func shareAction(_ sender: UIButton) {
+        
+        let shareController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        shareController.completionWithItemsHandler = {_, bool, _, _ in
+            if bool {
+                print("Success!")
+            }
+        }
+        
+        present(shareController, animated: true, completion: nil)
     }
     
 }
