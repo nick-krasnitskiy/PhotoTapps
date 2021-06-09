3b531aaad61a1df1adfd48ffc8bf4e1d723b3f1c
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-06-09 13:48:29 +0300

    CollectionView Cell customisation was done.

--- PhotoTapps/PhotosCollectionViewController.swift
+++ PhotoTapps/PhotosCollectionViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class PhotosCollectionViewController: UICollectionViewController {
 
-    let itemsPerRow: CGFloat = 2
+    let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10", "dog11", "dog12", "dog13", "dog14", "dog15",]
+    
+    let itemsPerRow: CGFloat = 3
     let sectionInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
     
     override func viewDidLoad() {
@@ -26,12 +28,15 @@ class PhotosCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 15
+        return photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
-        cell.backgroundColor = .black
+        let imageName = photos[indexPath.item]
+        let image = UIImage(named: imageName)
+        cell.dogImageView.image = image
+        
         return cell
     }
     
